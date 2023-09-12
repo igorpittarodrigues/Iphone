22a8904414e7a8b5987d134d240c2e258c0d85a9
Igor Pitta Rodrigues <rock1141@gmail.com>
2023-09-12 13:31:22 -0300

    first commit

---
+++ src/telefone/Iphone.java
@@ -0,0 +1,26 @@
+package telefone;
+
+public class Iphone implements AparelhoTelefonico {
+
+
+    
+
+    @Override
+    public void ligar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'ligar'");
+    }
+
+    @Override
+    public void atender() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atender'");
+    }
+
+    @Override
+    public void atenderCorreioVoz() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atenderCorreioVoz'");
+    }
+    
+}
